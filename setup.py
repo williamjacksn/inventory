9fb4e47d26a90d5e34c6d6108caed58344bbac3a
William Jackson <william@subtlecoolness.com>
2017-04-10 21:39:53 -0500

    Add a requirement to setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='inventory',
-    version='1.0.0',
+    version='1.0.1',
     description='A basic inventory management web app',
-    install_requires=['Flask', 'psycopg2', 'waitress'],
+    install_requires=['Flask', 'Flask-OAuth2-Login', 'psycopg2', 'waitress'],
     packages=['inventory'],
     include_package_data=True,
     entry_points={
