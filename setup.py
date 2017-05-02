3a9fd8c360cf5e376cb0506497e3a1ca2bb1335a
William Jackson <william@subtlecoolness.com>
2017-05-02 14:20:39 -0500

    Add check_stock script

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='inventory',
-    version='1.0.4',
+    version='1.0.5',
     description='A basic inventory management web app',
-    install_requires=['Flask', 'Flask-OAuth2-Login', 'Flask-SSLify', 'psycopg2', 'waitress'],
+    install_requires=['Flask', 'Flask-OAuth2-Login', 'Flask-SSLify', 'lxml', 'psycopg2', 'waitress'],
     packages=['inventory'],
     include_package_data=True,
     entry_points={
         'console_scripts': [
+            'check_stock = inventory.check_stock:main',
             'inventory = inventory.inventory:main'
         ]
     }
