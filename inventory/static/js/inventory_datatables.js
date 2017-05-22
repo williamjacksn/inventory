768f4ea483ad87d2528104f4c1b3579733ed4d88
William Jackson <william@subtlecoolness.com>
2017-05-22 12:20:08 -0500

    Turn off automatic table ordering

--- inventory/static/js/inventory_datatables.js
+++ inventory/static/js/inventory_datatables.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $('.datatable').DataTable({
         fixedHeader: true,
         info: false,
+        order: [],
         paging: false,
         searching: false
     });
